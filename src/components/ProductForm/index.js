87efc4e639cd4496e254a99ca7eef0afe77d99cd
Chadd Poggenpoel <chaddpoggenpoel@gmail.com>
2020-05-22 10:05:55 +0200

    0.0.1

--- src/components/ProductForm/index.js
+++ src/components/ProductForm/index.js
@@ -4,14 +4,6 @@ import isEqual from 'lodash/isEqual'
 import PropTypes from 'prop-types'
 
 import StoreContext from '~/context/StoreContext'
-import {
-  StyledPrice,
-  StyledLabel,
-  StyledSelect,
-  Holder,
-  MiniHolder,
-  Button
-} from './styles'
 
 const ProductForm = ({ product }) => {
   const {
@@ -74,13 +66,13 @@ const ProductForm = ({ product }) => {
     addVariantToCart(productVariant.shopifyId, quantity)
   }
 
-  /* 
-  Using this in conjunction with a select input for variants 
-  can cause a bug where the buy button is disabled, this 
+  /*
+  Using this in conjunction with a select input for variants
+  can cause a bug where the buy button is disabled, this
   happens when only one variant is available and it's not the
-  first one in the dropdown list. I didn't feel like putting 
+  first one in the dropdown list. I didn't feel like putting
   in time to fix this since its an edge case and most people
-  wouldn't want to use dropdown styled selector anyways - 
+  wouldn't want to use dropdown styled selector anyways -
   at least if the have a sense for good design lol.
   */
   const checkDisabled = (name, value) => {
@@ -103,16 +95,13 @@ const ProductForm = ({ product }) => {
     style: 'currency',
   }).format(variant.price)
 
-
-
   return (
     <>
-      <StyledPrice>{price}</StyledPrice>
+      <h3>{price}</h3>
       {options.map(({ id, name, values }, index) => (
         <React.Fragment key={id}>
-          <br />
-          <StyledLabel htmlFor={name}>{name}</StyledLabel>
-          <StyledSelect
+          <label htmlFor={name}>{name} </label>
+          <select
             name={name}
             key={id}
             onChange={event => handleOptionChange(index, event)}
@@ -126,31 +115,28 @@ const ProductForm = ({ product }) => {
                 {value}
               </option>
             ))}
-          </StyledSelect>
+          </select>
           <br />
         </React.Fragment>
       ))}
-      <Holder>
-        <StyledLabel htmlFor="quantity">Quantity</StyledLabel>
-        <MiniHolder>
-          <input
-            type="number"
-            id="quantity"
-            name="quantity"
-            min="1"
-            step="1"
-            onChange={handleQuantityChange}
-            value={quantity}
-          />
-            <Button
-            type="submit"
-            disabled={!available || adding}
-            onClick={handleAddToCart}
-          >
-            Add to Cart
-          </Button>
-        </MiniHolder>
-      </Holder>
+      <label htmlFor="quantity">Quantity </label>
+      <input
+        type="number"
+        id="quantity"
+        name="quantity"
+        min="1"
+        step="1"
+        onChange={handleQuantityChange}
+        value={quantity}
+      />
+      <br />
+      <button
+        type="submit"
+        disabled={!available || adding}
+        onClick={handleAddToCart}
+      >
+        Add to Cart
+      </button>
       {!available && <p>This Product is out of Stock!</p>}
     </>
   )
